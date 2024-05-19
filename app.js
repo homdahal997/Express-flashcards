8cabed6d7367b110075433780f2735fe032474b4
Hom Dahal <homdahal997@gmail.com>
2024-05-18 21:49:19 -0400

    Pug templating added to the project

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get("/", (req, res) =>{
     res.send("<h1>I Love Team Tree House. It is a good coding teaching platform</h1>");
 });
