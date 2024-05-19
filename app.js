2229d38bbc16dc544940289dc1b3eaee18a41115
Hom Dahal <homdahal997@gmail.com>
2024-05-18 21:30:29 -0400

    Hello route added

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) =>{
-    response.send("I Love Team Tree House. It is a good coding traching platform");
+app.get("/", (req, res) =>{
+    res.send("<h1>I Love Team Tree House. It is a good coding teaching platform</h1>");
 });
 
-app.listen(3000);
+app.get("/hello", (req, res) =>{
+    res.send("<h1>Hello Javascript Developer</h1>");
+});
+
+app.listen(3000, () => {
+    console.log("The application is running localhost:3000");
+});
