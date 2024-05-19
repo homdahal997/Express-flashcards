f91b3760586e8afd427325dd95c4ff28a59ecff6
Hom Dahal <homdahal997@gmail.com>
2024-05-18 22:23:02 -0400

    pug file added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get("/", (req, res) =>{
-    res.send("<h1>I Love Team Tree House. It is a good coding teaching platform</h1>");
+    res.render('index');
 });
 
 app.get("/hello", (req, res) =>{
